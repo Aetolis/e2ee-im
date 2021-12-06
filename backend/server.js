58497f0a1a70cee900bac20765882ea7c9e9eec1
Aetolis <12118983+Aetolis@users.noreply.github.com>
2021-12-06 18:03:25 -0500

    Add prisma to server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 import { WebSocketServer } from 'ws';
+import Prisma from '@prisma/client';
+
+const { PrismaClient } = Prisma;
+const prisma = new PrismaClient()
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const user = await prisma.user.create({
+  data: {
+    IK_B: new Buffer.from("!", 'utf8'),
+    SPK_B: new Buffer.from("!", 'utf8'),
+    Sig: new Buffer.from("!", 'utf8')
+  },
+})
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    console.log('received: %b', data);
+    
   });
 
   ws.send('something');
